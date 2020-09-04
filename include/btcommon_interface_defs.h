5c285422df13c69b416e35dcb56b3ecf83011efe
Rodrigo Martinez <rear097@gmail.com>
2020-09-04 15:19:34 +0200

    Revert "Add support for Moselle SoC"
    This reverts commit a3f254d0c71a506204d867e2e916f1f47ed21560.

--- include/btcommon_interface_defs.h
+++ include/btcommon_interface_defs.h
@@ -39,7 +39,7 @@ typedef enum {
   BT_SOC_TYPE_ROME,
   BT_SOC_TYPE_CHEROKEE,
   BT_SOC_TYPE_HASTINGS,
-  BT_SOC_TYPE_MOSELLE,
+  BT_SOC_TYPE_NAPIER,
   /* Add chipset type here */
   BT_SOC_TYPE_RESERVED
 } bt_soc_type_t;
